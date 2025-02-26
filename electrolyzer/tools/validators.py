2522fc0d0c4b17e9068fd66377ad5bf9c8e598ec
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2025-02-26 11:52:57 -0700

    updated validator format

--- electrolyzer/tools/validators.py
+++ electrolyzer/tools/validators.py
@@ -2,6 +2,7 @@
 This module contains validator functions for use with `attrs` class definitions.
 """
 
+
 def gt_zero(instance, attribute, value):
     """Validates that an attribute's value is greater than zero."""
     if value <= 0:
@@ -9,7 +10,9 @@ def gt_zero(instance, attribute, value):
 
 
 def range_val(min, max):
-    """Validates that an attribute's value is between two values, inclusive ([min, max])."""
+    """Validates that an attribute's value is between two values,
+    inclusive ([min, max])."""
+
     def validator(instance, attribute, value):
         if value < min or value > max:
             raise ValueError(f"{attribute} must be in range [{min}, {max}]")
@@ -19,6 +22,7 @@ def range_val(min, max):
 
 def contains(items):
     """Validates that an item is part of a given list."""
+
     def validator(instance, attribute, value):
         if value not in items:
             raise ValueError(f"Item {value} not found in list for {attribute}: {items}")
