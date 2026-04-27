9363d499ba1f7e79e4d3c533426abae655692e4f
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2026-04-27 14:56:31 -0600

    added files to support new framework

---
+++ electrolyzer/components/cell/cell.py
@@ -0,0 +1,37 @@
+import openmdao.api as om
+
+
+class StackBaseClass(om.ExplicitComponent):
+    def initialize(self):
+        self.options.declare("plant_config", types=dict)
+        self.options.declare("tech_config", types=dict)
+
+    def setup(self):
+        # design variables
+        self.add_input("cell_active_area", units="A/(cm**2)")
+        self.add_input("membrane_thickness", units="cm")
+        self.add_input("operating_temperature", units="degC")
+        self.add_input("anode_pressure", units="bar")
+        self.add_input("cathode_pressure", units="bar")
+        self.add_input("R_elec", units="ohm*(cm**2)")
+
+        # input profiles
+        self.add_input("current", val=0.0, shape=self.n_timesteps, units="A")
+
+        # output profiles
+        self.add_output("cell_voltage")
+        self.add_output("h2_produced")
+        self.add_output("o2_produced")
+        self.add_output("h2o_consumed")
+        self.add_output("h2_production_rate")
+
+        # output design variables
+
+    def compute(self, inputs, outputs, discrete_inputs, discrete_outputs):
+        """
+        Computation for the OM component.
+
+        For a template class this is not implement and raises an error.
+        """
+
+        raise NotImplementedError("This method should be implemented in a subclass.")
